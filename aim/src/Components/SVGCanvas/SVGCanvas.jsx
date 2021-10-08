9710bf92e6f328ef42c99f30e311a1894e28ad9c
kotofeyy <shreddikenot@gmail.com>
2021-10-09 00:03:32 +0300

    added remove balls and console

--- aim/src/Components/SVGCanvas/SVGCanvas.jsx
+++ aim/src/Components/SVGCanvas/SVGCanvas.jsx
@@ -2,23 +2,47 @@ import React from "react";
 import "./SVGCanvas.css";
 
 const generateValue = () => {
-  return Math.round(10 + Math.random() * 980);
+  return Math.round(20 + Math.random() * 960);
 };
-const anyData = Array.from({ length: 50 }, (_el, index) => ({
-  label: index++,
-  cx: generateValue(),
-  cy: generateValue(),
-}));
 
 function SVGCanvas() {
+  const [balls, setBalls] = React.useState(
+    Array.from({ length: 50 }, (_el, index) => ({
+      key: index++,
+      cx: generateValue(),
+      cy: generateValue(),
+    }))
+  );
+
+  const removeBalls = (i) => {
+    setBalls(balls.filter((item, j) => i !== j));
+  };
   return (
     <svg
       className="svg-canvas"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1000 500"
     >
-      {anyData?.map((item) => {
-        return <circle cx={item.cx} cy={item.cy} r="10" />;
+      <g className="svg-canvas__console">
+        {balls.map((item, i) => {
+          return (
+            <text x="10" y={i * 7 + 10} fontSize="8px">
+              balls-{item.key}
+            </text>
+          );
+        })}
+      </g>
+      {balls?.map((item, i) => {
+        return (
+          <>
+            <circle
+              cx={item.cx}
+              cy={item.cy}
+              r="10"
+              onClick={() => removeBalls(i)}
+            />
+          </>
+        );
       })}
     </svg>
   );
