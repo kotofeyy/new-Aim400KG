b11e0837dc04624305268874cd0ad827c2f8b1c5
kotofeyy <shreddikenot@gmail.com>
2021-11-02 04:44:01 +0300

    малюсенькие изменения

--- aim/src/App.js
+++ aim/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Wrapper title="">
+        <Wrapper title="Exact Aiming">
           <SVGCanvas />
         </Wrapper>
       </header>
--- aim/src/Components/Wrapper/Wrapper.jsx
+++ aim/src/Components/Wrapper/Wrapper.jsx
@@ -3,12 +3,12 @@ import { useSelector } from "react-redux";
 import close from "./images/close.png";
 import "./Wrapper.css";
 
-function Wrapper({ children }) {
+function Wrapper({ children, title }) {
   const aiming = useSelector((state) => state.exactAiming.ballInfo);
   return (
     <div className="wrapper">
       <div className="wrapper__header">
-        <div className="wrapper__name">Exact Aiming</div>
+        <div className="wrapper__name">{title}</div>
         <div className="wrapper__game_performance">{aiming}</div>
         <div className="wrapper__close">
           <img src={close} width="20" />
