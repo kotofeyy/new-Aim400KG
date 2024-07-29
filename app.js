243c54a1b733edc8e23edb40974565e2a30e834f
nimoo <nimo@mail.yo>
2024-07-30 01:28:21 +0700

    засекание времени + среднее

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-let svg = document.querySelector('.demo');
-let circle = document.createElementNS(
-    'http://www.w3.org/2000/svg',
-    'circle'
-);
-console.log(svg)
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+const CIRCLE_RADIUS = 40;
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
-
 const colorList = [
     '#9933FF',
     '#6633FF',
@@ -17,16 +11,51 @@ const colorList = [
     '#FF6666',
     '#FF6600',
 ];
+let timerStart, timerEnd;
+let isFirst = true;
+
+let averageArr = [];
+
+function timer() {
+    if (isFirst) {
+        timerStart = new Date().valueOf();
+        isFirst = false;
+    } else {
+        timerEnd = new Date().valueOf();
+        document.querySelector('.seconds').textContent = `${
+            timerEnd - timerStart
+        } ms`;
+        averageArr.push(timerEnd - timerStart);
+        timerStart = new Date().valueOf();
+    }
+}
+
+function showAverage() {
+    document.querySelector('.average').textContent = `${Math.floor(
+        averageArr.reduce((acc, number) => acc + number, 0) / averageArr.length
+    )} ms`;
+}
+
+let svg = document.querySelector('.demo');
+let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
 function spawnCircle() {
-    circle.setAttribute('cx', getRandomInt(svg.clientWidth))
-    circle.setAttribute('cy', getRandomInt(svg.clientHeight))
-    circle.setAttribute('fill', colorList[getRandomInt(6)]);
-    circle.setAttribute('r', 40);
+    circle.setAttribute(
+        'cx',
+        getRandomInt(CIRCLE_RADIUS * 2, svg.clientWidth - CIRCLE_RADIUS)
+    );
+    circle.setAttribute(
+        'cy',
+        getRandomInt(CIRCLE_RADIUS * 2, svg.clientHeight - CIRCLE_RADIUS)
+    );
+    circle.setAttribute('fill', colorList[getRandomInt(0, 6)]);
+    circle.setAttribute('r', CIRCLE_RADIUS);
 }
 
 circle.addEventListener('click', () => {
+    timer();
     spawnCircle();
+    showAverage();
 });
 spawnCircle();
 document.querySelector('.demo').appendChild(circle);
